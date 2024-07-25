e2feffc812949a08acfe0bdfcea8cc1cd4ceec47
SubhashiniVenkat <vbhashi@gmail.com>
2024-07-25 14:38:02 +0530

    Add files via upload
    Day 1

---
+++ Firefox.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Firefox 
+{
+	public static void main(String[] args) 
+	{
+	float browserVersion = 100.2f;
+	String browserName = "firefox";
+	boolean isVisible = true;
+	int releaseYear = 1998;
+	char browserLogo = 'f';
+	System.out.println("The browser version :"+browserVersion+ "\n" + "The browser name :" +browserName+ "\n" + "The browser visibility :" +isVisible+ "\n" + "The browser release year :" +releaseYear + "\n"+ "The browser logo : " +browserLogo);
+	}		
+}
